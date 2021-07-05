56f2d03963327dd5014cf3e630656ee223f56b91
Arturo de la Cruz <arturodlcl@gmail.com>
2021-07-05 09:38:31 +0100

    update global add fn

--- inst/app/global.R
+++ inst/app/global.R
@@ -6,4 +6,43 @@ selected_cols <- c("NCTId","StudyType","OverallStatus","StartDate",
 # clean enrollment count to set slider
 enrolled <- df$EnrollmentCount[which(!is.na(df$EnrollmentCount))]
 
+#' To Intervention Arms Table
+#'
+#' Takes data frame with studies restults and returns dataframe with one row per study
+#' and split intervention arms.
+#' armify()
+#' @param df a dataframe to manipulate
+#' @examples
+#' armify(df)
+
+
+armify <- function(df) {
+
+  af <- df %>% select(NCTId, EnrollmentCount, OverallStatus, Phase, IsFDARegulatedDrug, HasResults, ArmGroupInterventionName) %>% filter(!is.na(ArmGroupInterventionName))
+  lab_names <- paste("label",seq(1:50),sep = "")
+
+  make_wide <- separate(af, col = ArmGroupInterventionName,
+                        into = lab_names, sep = '\\|',
+                        remove = TRUE, extra = "merge", fill = "right",
+                        convert = TRUE) %>%
+    select_if(~!(all(is.na(.)) | all(. == "")))
+
+  make_character<- mutate(make_wide, across(everything(),as.character))
+
+  make_long <- pivot_longer(make_character,
+                            !c("NCTId", "EnrollmentCount", "OverallStatus","Phase", "IsFDARegulatedDrug", "HasResults"),
+                            values_drop_na = T)
+
+  drop_colon <- gsub(".*:","",make_long$value)
+
+  bind_new <- cbind(make_long[1:7],drop_colon)
+
+  remake_wide <- pivot_wider(bind_new, names_from = name, values_from = drop_colon)
+
+  # grouping all placebos
+  remake_wide$label1 <- ifelse(grepl("placebo",remake_wide$label1,ignore.case = T),"Any Placebo", remake_wide$label1)
+  remake_wide$label2 <- ifelse(grepl("placebo",remake_wide$label2,ignore.case = T),"Any Placebo", remake_wide$label2)
+
+  return(remake_wide) # or make_long
+}
 
