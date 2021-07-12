8f811d7bd8f765353fffca8f553923fcbaf04b86
Arturo de la Cruz <arturodlcl@gmail.com>
2021-07-12 15:27:12 +0100

    update selcted cols and delete man for deleted function

--- inst/app/global.R
+++ inst/app/global.R
@@ -1,3 +1,7 @@
 
 # clean enrollment count to set slider
 enrolled <- df$EnrollmentCount[which(!is.na(df$EnrollmentCount))]
+
+selected_cols <- c("NCTId","StudyType","OverallStatus","StartDate",
+                   "CompletionDate","Condition","Phase","EnrollmentCount","ArmGroupType","InterventionType",
+                   "InterventionMeshTerm","Gender","AgeRange","HealthyVolunteers")
